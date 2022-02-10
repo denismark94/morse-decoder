4a68616a9458ce20469c82c1d5efcef0fa0f0120
denismark94 <denismark94@gmail.com>
2022-02-10 20:02:22 +0300

    feat: implement morse decoding

--- src/index.js
+++ src/index.js
@@ -38,7 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let res = '';
+    for (let i = 0; i < expr.length; i+=10) {
+        let letter = expr.substring(i, i + 10);
+        if (letter === '**********') {
+            res += ' ';
+            continue;
+        }
+        let code = '';
+        for (let j = 0; j < 10; j += 2) {
+            switch (letter.substring(j,j + 2)) {
+                case '11':
+                    code += '-';
+                    break;
+                case '10':
+                    code += '.';
+                    break;
+            }
+        }
+        res += MORSE_TABLE[code]
+    }
+    return res;
 }
 
 module.exports = {
